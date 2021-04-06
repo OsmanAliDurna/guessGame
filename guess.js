bdaac601e93fa0ddfbcae38ed5d0bdd76d8e8be0
Osman Ali Durna <o_durna@hotmail.com>
2021-04-06 09:26:40 +0300

    updated

--- guess.js
+++ guess.js
@@ -23,7 +23,10 @@ function counter() {
 
 function isNumberKey(evt) {
   var charCode = evt.which ? evt.which : evt.keyCode;
-  if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
+  if (charCode == 13) {
+    compare();
+  }
+  if (charCode < 48 || charCode > 57) return false;
   if (inputNumber.value.length > 1) {
     inputNumber.value = 100;
     return false;
@@ -32,6 +35,7 @@ function isNumberKey(evt) {
 }
 
 const div = document.querySelector("div");
+const guessText = document.getElementsByTagName("label")[0];
 const inputNumber = document.getElementById("number");
 const guessButton = document.querySelector("#guess");
 const infoMessage = document.querySelector("h2");
@@ -40,6 +44,7 @@ const ranNum = Math.floor(Math.random() * 101);
 
 //guessButton.addEventListener("click", timer); opsiyonel
 //guessButton.removeEventListener("click", changeColor); bakılacak.
+
 guessButton.addEventListener("click", compare);
 console.log(ranNum);
 
@@ -49,6 +54,19 @@ let lastSelected = -1;
 let attemptCounter = 0;
 infoMessage.innerHTML = "Please enter a Number Over There!";
 
+// Text styling needed
+////////////////////////////////////////////////////////
+
+const colorful = guessText.textContent.trim();
+
+const r = Math.trunc(Math.random() * 255);
+const g = Math.trunc(Math.random() * 255);
+const b = Math.trunc(Math.random() * 255);
+
+guessText.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+////////////////////////////////////////////////////////
+
 function compare() {
   let userGuess = parseInt(inputNumber.value);
 
@@ -56,23 +74,26 @@ function compare() {
     infoMessage.innerHTML = "Hey, I think you miss something ?";
   } else if (lastSelected == 1 && userGuess <= lastGuess && lastGuess >= 0) {
     infoMessage.innerHTML =
-      "No no no! I said bigger <br> I don't count this one.";
+      "No no no! I said <u><i>bigger</i></u> <br> I don't count this one.";
     attemptCount.innerHTML = "Your Lucky Day";
+    inputNumber.value = "";
   } else if (lastSelected == 0 && userGuess >= lastGuess) {
     infoMessage.innerHTML =
-      "No no no! I said smaller <br> I don't count this one.";
+      "No no no! I said <u><i>smaller</i></u> <br> I don't count this one.";
     attemptCount.innerHTML = "Your Lucky Day";
+    inputNumber.value = "";
   } else {
     lastGuess = userGuess;
     if (userGuess > ranNum) {
-      infoMessage.innerHTML = "Please Guess again <br> But guess smaller now";
+      infoMessage.innerHTML =
+        "Please Guess again <br> But guess <u><i>smaller</i></u> now";
       attemptCounter += 1;
       inputNumber.value = "";
       attemptCount.innerHTML = "Attempts: " + attemptCounter;
       lastSelected = 0;
     } else if (userGuess < ranNum) {
       infoMessage.innerHTML =
-        "Please Guess again <br> But guess bigger this time";
+        "Please Guess again <br> But guess <u><i>bigger</i></u> this time";
       attemptCounter += 1;
       inputNumber.value = "";
       attemptCount.innerHTML = "Attempts: " + attemptCounter;
@@ -84,6 +105,8 @@ function compare() {
       attemptCounter += 1;
       attemptCount.innerHTML = "Attempts: " + attemptCounter;
       guessButton.disabled = true;
+      inputNumber.disabled = true;
+      inputNumber.value = "";
       guessButton.textContent = "This Turn Finished";
       timer();
     }
